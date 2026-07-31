bd4abe3ad6b716199b679e9dc44f664bcfcc7f50
Yash Sen <yashsen0402@gmail.com>
2026-07-31 18:57:20 +0530

    Updated: minor updated for docker

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -2,7 +2,7 @@ import { getToken, removeToken } from "@/utils/token";
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5000",
+    baseURL: "http://localhost:5000",
     headers: {
     "Content-Type": "application/json",
     },
